3b1d5ca17aa9d31a5fde00935dc4cef09d030ad3
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-04-01 18:02:53 -0300

    feat: limpando css

---
+++ src/components/Layouts/Header.jsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <Link>
+                    Logo
+                </Link>
+            </div>
+        </header>
+    )
+}
+export default Header;
