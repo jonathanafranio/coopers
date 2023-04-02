d6fe1717bfe279e734da0e568e5412eab6c869f5
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-04-02 01:31:02 -0300

    feat: footer

---
+++ src/components/Layouts/Footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <p className="footer__text">
+                    Need help?
+                    <br />
+                    <a href="mailto:coopers@coopers.pro">coopers@coopers.pro</a>
+                </p>
+                <span className="footer__copy">© 2021 Coopers. All rights reserved.</span>
+            </div>
+        </footer>
+    )
+}
+export default Footer
--- src/components/Layouts/Layouts.tsx
+++ src/components/Layouts/Layouts.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Header from "./Header"
+import Footer from "./Footer"
 
 const Layouts = (props: object) => {
     return(
@@ -8,6 +9,7 @@ const Layouts = (props: object) => {
             <main>
                 { props.children }
             </main>
+            <Footer />
         </>
     )
 }
