d89420bec043ae94816b9c4cd6bb24691da36045
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-04-02 23:17:49 -0300

    feat: markup modal de login

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,17 +1,22 @@
 import Link from "next/link"
 import Image from "next/image"
 import Logo from "../../assets/img/logo.png"
+import ModalLogin from '../ModalLogin'
+import { useState } from "react"
 
 const Header = () => {
+    const [modalShow, setModalShow] = useState(false)
     return (
         <header className="header">
             <Link href={ '/' } className="header__logo">
                 <Image src={ Logo } alt="Coopers" />
             </Link>
 
-            <button className="header__btn">
+            <button className="header__btn" onClick={ e => setModalShow(true) }>
                 entrar
             </button>
+
+            { modalShow ? (<ModalLogin toggleModal={ setModalShow } />) : false }
         </header>
     )
 }
---
+++ src/components/ModalLogin.jsx
@@ -0,0 +1,34 @@
+const ModalLogin = (props) => {
+    const { toggleModal } = props;
+
+    window.addEventListener('keydown', () => toggleModal(false));
+
+    return(
+        <div className="modal-login">
+            <div className="modal-login__content">
+                <button className="modal-login__close" onClick={ e => toggleModal(false) }>close</button>
+                <form action="" className="modal-login__form">
+                    <h2 className="modal-login__title">
+                        <strong>Sign in</strong> <br />
+                        to access your list
+                    </h2>
+
+                    <div className="modal-login__field">
+                        <label htmlFor="user-name">User:</label>
+                        <input type="text" name="user-name" id="user-name" />
+                    </div>
+
+                    <div className="modal-login__field">
+                        <label htmlFor="user-pass">Password::</label>
+                        <input type="password" name="user-pass" id="user-pass" />
+                    </div>
+
+                    <button className="modal-login__btn">Sign in</button>
+
+                </form>
+                <div className="modal-login__ico"></div>
+            </div>
+        </div>
+    )
+}
+export default ModalLogin
