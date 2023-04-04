c7b51d949410438f45dc2dc0f114c314631ee0ab
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-04-03 23:39:08 -0300

    feat: endpoint de login

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -15,13 +15,17 @@ export const login = (req, res) => {
     const { body } = req;
     const { user, password } = body;
     
-    console.log({ body })
-    const q = `SELECT * FROM users WHERE name = '${ user }';`;
+    //const q = `SELECT * FROM users WHERE name = '${ user }' AND password = MD5('${password}');`;
+    const q = `SELECT id, name, email, phone FROM users WHERE name = '${ user }' AND password = MD5('${password}');`;
+    //const q = `SELECT * FROM users WHERE name = '${ user }';`;
     
     db.query(q, (err, data) => {
         if(err) return res.json(err);
 
-        return res.status(200).json(data);
-
+        if(data.length > 0) {
+            return res.status(200).json(data);
+        } else {
+            return req.status(200).json('Usuario ou senha errada.');
+        }
     })
 }
