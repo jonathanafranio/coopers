60aec15d89cf2fc4521001415b3cbfa778dc9c1f
jonathanafranio <jonathan.afranio.s@gmail.com>
2023-04-08 02:46:26 -0300

    chore: remove alguns comentarios desnecessarios

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -4,7 +4,6 @@ export const login = (req, res) => {
     const { body } = req;
     const { user, password } = body;
     
-    //const q = `SELECT * FROM users WHERE name = '${ user }' AND password = MD5('${password}');`;
     const q = `SELECT id, name, email FROM users WHERE name = '${ user }' AND password = MD5('${password}');`;
     
     db.query(q, (err, data) => {
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,6 @@
 import { db } from '../db.js';
 
 export const getUsers = (_, res) => {
-    //return res.status(200).json({ oi: 'testes' });
     const q = 'SELECT * FROM users';
 
     db.query(q, (err, data) => {
